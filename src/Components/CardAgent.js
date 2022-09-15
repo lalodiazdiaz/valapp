1bebf3cbd9f20182264a3a6f39b8844b61c9ca63
lalodiazdiaz <eduardodiazdiaz93@gmail.com>
2022-09-14 21:44:49 -0500

    alt img

--- src/Components/CardAgent.js
+++ src/Components/CardAgent.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import styles from '../Styles/Agents.module.css'
-
+import React from "react";
+import styles from "../Styles/Agents.module.css";
 
 const CardAgent = (props) => {
+  const data = props.data;
 
-const data = props.data
-  
   return (
     <div className={styles.containerCard}>
-    <img className={styles.imgCard} src={data.displayIcon} />
-    <h1>{data.displayName}</h1>
+      <img className={styles.imgCard} src={data.displayIcon} alt={"agente"} />
+      <h1>{data.displayName}</h1>
     </div>
+  );
+};
 
-  )
-}
-
-export default CardAgent
+export default CardAgent;
